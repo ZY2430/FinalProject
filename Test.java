3804b8447c797806f8a4e1e16f2975ed334198cf
ZhiyuanXie <42878795+ZhiyuanXie@users.noreply.github.com>
2019-11-20 21:05:10 -0800

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,10 +1,14 @@
 package wuzi;
-
+/**
+*This clss is for testing the connect 6 game code.
+*Author Group 5
+*CS151
+*/
 
 public class Test {
 	
 	public static void main(String[] args) { 
-		  StartChessJFrame f=new StartChessJFrame();//create the frame of the game
+		  StartChessJFrame f=new StartChessJFrame();//construct the frame of the game
 		  f.setVisible(true);//show the frame
 		 } 
 }
