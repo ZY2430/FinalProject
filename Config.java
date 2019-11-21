f2c609e7fe9cd73863040ec94f510e0a81d747fb
ZhiyuanXie <42878795+ZhiyuanXie@users.noreply.github.com>
2019-11-21 13:31:05 -0800

    add Javadoc

--- Config.java
+++ Config.java
@@ -2,12 +2,15 @@ package wuzi;
 
 import java.awt.Dimension;
 
+/**
+*This interface stores the four major numbers
+*/
 public interface Config {  
 	public static int MARGIN=30;//The value of the marginal distance
 	 public static int GRID_SPAN=35;//the value of the sepparation distance between griddings
-    int ROWS = 15;  // number of rows
+    int ROWS = 19;  // the number of rows
       
-    int COLS = 15;  // number of rows
+    int COLS = 19;  //  the number of rows
       
   
 }  
