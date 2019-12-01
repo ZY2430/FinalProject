a7ba41e3d96d8808890bd4fa033403e0c1c088dc
ZhiyuanXie <42878795+ZhiyuanXie@users.noreply.github.com>
2019-12-01 15:28:00 -0800

    Update Point.java

--- Point.java
+++ Point.java
@@ -42,17 +42,22 @@ public class Point implements Serializable {
     /**
      *
      */
-    private int x;//棋子在棋盘中的x索引值
-    private int y;//棋子在棋盘中的y索引值
-    private Color color;//颜色
-    public static int DIAMETER = 30;//直径
+    private int x;//the x coordinate 
+    private int y;//the y coordinate
+    private Color color;//the color of chess white or black
+    public static int DIAMETER = 30;//the diameter
 
+    /**
+     * construct the point 
+     * @param x the value of x coordinate
+     * @param y the value of y coordinate
+     * @param color the color of chess
+     */
     public Point(int x, int y, Color color) {
         this.x = x;
         this.y = y;
         this.color = color;
     }
-    //得到棋子在棋盘中的x索引值
 
     /**
      * Get the x coordinate
@@ -62,16 +67,21 @@ public class Point implements Serializable {
     public int getX() {
         return x;
     }
-    //得到棋子在棋盘中的y索引值
+
 
     /**
-     * @return y the
+     * Get the y coordinate
+     *
+     * @return y the value of the vertical coordinate of chess
      */
     public int getY() {
         return y;
     }
 
-    //得到棋子颜色
+    /**
+     * get the color of chess
+     * @return the color of chess
+     */
     public Color getColor() {
         return color;
     }
