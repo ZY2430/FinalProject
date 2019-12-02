de74f65fd10558728b15ff89a2fbac2021bed031
hao tu <44190561+tuhaocfs@users.noreply.github.com>
2019-12-02 10:08:50 -0800

    Update ChessBoard.java

--- ChessBoard.java
+++ ChessBoard.java
@@ -9,24 +9,24 @@ import java.awt.event.MouseEvent;
 /*五子棋-棋盘类*/ 
 public class ChessBoard extends JPanel implements MouseListener{ 
 
- Point[] chessList=new Point[(Config.ROWS+1)*(Config.COLS+1)];//初始化每个数组元素为null 
- boolean isBack=true;//默认开始是黑棋先下 
- boolean gameOver=false;//游戏是否结束 
- int chessCount;//当前棋盘的棋子个数 
- int xIndex,yIndex;//当前刚下棋子的索引 
+ Point[] chessList=new Point[(Config.ROWS+1)*(Config.COLS+1)];//Initialize each array element to null
+ boolean isBack=true;//Black chessmen first by default 
+ boolean gameOver=false;//Whether the game is over 
+ int chessCount;//Number of chessmen on the current board 
+ int xIndex,yIndex;//The index of the current chessmen
  public ChessBoard(){ 
-  setBackground(Color.LIGHT_GRAY);//设置背景颜色为黄色 
-  addMouseListener(this);//添加事件监听器 
-  addMouseMotionListener(new MouseMotionListener() {//匿名内部类 
+  setBackground(Color.LIGHT_GRAY);//Set the background color to yellow 
+  addMouseListener(this);//Add event listener
+  addMouseMotionListener(new MouseMotionListener() {//Anonymous inner class
      
    @Override 
    public void mouseMoved(MouseEvent e) { 
     int x1=(e.getX()-Config.MARGIN+Config.GRID_SPAN/2)/Config.GRID_SPAN; 
-    int y1=(e.getY()-Config.MARGIN+Config.GRID_SPAN/2)/Config.GRID_SPAN;//将鼠标单击的坐标位置转化为网格索引 
-    if(x1<0||x1>Config.ROWS||y1<0||y1>Config.COLS||gameOver||findChess(x1,y1)){//游戏已经结束，不能下；落在棋盘外，不能下；x，y位置已经有棋子存在，不能下 
-     setCursor(new Cursor(Cursor.DEFAULT_CURSOR));//设置成默认形状 
+    int y1=(e.getY()-Config.MARGIN+Config.GRID_SPAN/2)/Config.GRID_SPAN;//Convert mouse click coordinate position to grid index 
+    if(x1<0||x1>Config.ROWS||y1<0||y1>Config.COLS||gameOver||findChess(x1,y1)){
+     setCursor(new Cursor(Cursor.DEFAULT_CURSOR));//Set to default shape 
     }else{ 
-     setCursor(new Cursor(Cursor.HAND_CURSOR));//设置成手型 
+     setCursor(new Cursor(Cursor.HAND_CURSOR));//
     } 
    } 
      
@@ -37,21 +37,21 @@ public class ChessBoard extends JPanel implements MouseListener{
  } 
  /*绘制*/ 
  public void paintComponent(Graphics g){ 
-  super.paintComponent(g);//画棋盘 
-  for(int i=0;i<=Config.ROWS;i++){//画横线 
+  super.paintComponent(g);//Draw chess board 
+  for(int i=0;i<=Config.ROWS;i++){//Draw horizontal lines
    g.drawLine(Config.MARGIN, Config.MARGIN+i*Config.GRID_SPAN, Config.MARGIN+Config.COLS*Config.GRID_SPAN, Config.MARGIN+i*Config.GRID_SPAN); 
   } 
-  for(int i=0;i<=Config.COLS;i++){//画直线 
+  for(int i=0;i<=Config.COLS;i++){//Draw a straight line
    g.drawLine(Config.MARGIN+i*Config.GRID_SPAN, Config.MARGIN, Config.MARGIN+i*Config.GRID_SPAN,Config.MARGIN+Config.ROWS*Config.GRID_SPAN); 
   } 
-  /*画棋子*/ 
+  /*Draw chessmen*/ 
   for(int i=0;i<chessCount;i++){ 
-   int xPos=chessList[i].getX()*Config.GRID_SPAN+Config.MARGIN;//网格交叉的x坐标 
-   int yPos=chessList[i].getY()*Config.GRID_SPAN+Config.MARGIN;//网格交叉的y坐标 
-   g.setColor(chessList[i].getColor());//设置颜色 
+   int xPos=chessList[i].getX()*Config.GRID_SPAN+Config.MARGIN;//X-coordinates of the grid crossing 
+   int yPos=chessList[i].getY()*Config.GRID_SPAN+Config.MARGIN;//Y coordinate of grid crossing 
+   g.setColor(chessList[i].getColor());//Set color 
    g.fillOval(xPos-Point.DIAMETER/2, yPos-Point.DIAMETER/2, Point.DIAMETER, Point.DIAMETER); 
    if(i==chessCount-1){ 
-    g.setColor(Color.red);//标记最后一个棋子为红色 
+    g.setColor(Color.red);//Mark the last chessman in red
     g.drawRect(xPos-Point.DIAMETER/2, yPos-Point.DIAMETER/2, Point.DIAMETER, Point.DIAMETER); 
    } 
   } 
@@ -60,28 +60,28 @@ public class ChessBoard extends JPanel implements MouseListener{
   
   
  @Override 
- public void mousePressed(MouseEvent e) {//鼠标按键在组件上按下时调用 
-  if(gameOver)//游戏已经结束，不能下 
+ public void mousePressed(MouseEvent e) {//Invoked when the mouse button is pressed on a component 
+  if(gameOver)//The game is over and cannot be played 
    return ; 
   String colorName=isBack ? "黑棋" : "白棋"; 
   xIndex=(e.getX()-Config.MARGIN+Config.GRID_SPAN/2)/Config.GRID_SPAN; 
-  yIndex=(e.getY()-Config.MARGIN+Config.GRID_SPAN/2)/Config.GRID_SPAN;//将鼠标单击的坐标位置转化为网格索引 
-  if(xIndex<0||xIndex>Config.ROWS||yIndex<0||yIndex>Config.COLS)//棋子落在棋盘外，不能下 
+  yIndex=(e.getY()-Config.MARGIN+Config.GRID_SPAN/2)/Config.GRID_SPAN;//Convert mouse click coordinate position to grid index 
+  if(xIndex<0||xIndex>Config.ROWS||yIndex<0||yIndex>Config.COLS)//Chessmen fall outside the chessboard and cannot be played
    return ; 
-  if(findChess(xIndex,yIndex))//x,y位置已经有棋子存在，不能下 
+  if(findChess(xIndex,yIndex))//There are already chessmen in the x and y positions.
    return ; 
     
   Point ch=new Point(xIndex,yIndex,isBack ? Color.black : Color.white); 
   chessList[chessCount++]=ch; 
-  repaint();//通知系统重新绘制 
+  repaint();//Notify the system to repaint 
   if(isWin()){ 
-   String msg=String.format("恭喜，%s赢啦~", colorName); 
+   String msg=String.format("Congratulations,% s ~ win the game", colorName); 
    JOptionPane.showMessageDialog(this, msg); 
    gameOver=true;   
   } 
   else if(chessCount==(Config.COLS+1)*(Config.ROWS+1)) 
   { 
-   String msg=String.format("棋鼓相当，棒棒哒~"); 
+   String msg=String.format("A Draw"); 
    JOptionPane.showMessageDialog(this,msg); 
    gameOver=true; 
   } 
@@ -89,19 +89,19 @@ public class ChessBoard extends JPanel implements MouseListener{
  } 
    
  @Override 
- public void mouseClicked(MouseEvent e) {//鼠标按键在组件上单击(按下并释放)时调用 
+ public void mouseClicked(MouseEvent e) {//Called when the mouse button is clicked (pressed and released) on a component 
  } 
   
  @Override 
- public void mouseReleased(MouseEvent e) {////鼠标按键在组件上释放时调用 
+ public void mouseReleased(MouseEvent e) {////Invoked when the mouse button is released on a component
  } 
   
  @Override 
- public void mouseEntered(MouseEvent e) {//鼠标进入组件时调用 
+ public void mouseEntered(MouseEvent e) {//Called when the mouse enters the component
  } 
   
  @Override 
- public void mouseExited(MouseEvent e){//鼠标离开组件时调用   
+ public void mouseExited(MouseEvent e){//Called when the mouse leaves the component  
  } 
    
  private boolean findChess(int x,int y){ 
@@ -112,81 +112,81 @@ public class ChessBoard extends JPanel implements MouseListener{
   return false; 
  } 
    
- /*判断那方赢*/ 
+ /*Judge which side wins the game*/ 
  private boolean isWin(){ 
-  int continueCount=1;//连续棋子的个数 
-  for(int x=xIndex-1;x>=0;x--){//横向向左寻找 
+  int continueCount=1;//Number of chessmen 
+  for(int x=xIndex-1;x>=0;x--){//Look left 
    Color c=isBack ? Color.black : Color.white; 
    if(getChess(x,yIndex,c)!=null){ 
     continueCount++; 
    }else 
     break; 
   } 
-  for(int x=xIndex+1;x<=Config.ROWS;x++){//横向向右寻找 
+  for(int x=xIndex+1;x<=Config.ROWS;x++){//Look right
    Color c=isBack ? Color.black : Color.white; 
    if(getChess(x,yIndex,c)!=null){ 
     continueCount++; 
    }else 
     break; 
   } 
-  if(continueCount>=5){//判断记录数大于等于五，即表示此方获胜 
+  if(continueCount>=6){//Judging that the number of records is greater than or equal to six, that means the party wins 
    return true; 
   }else 
    continueCount=1; 
   // 
-  for(int y=yIndex-1;y>=0;y--){//纵向向上寻找 
+  for(int y=yIndex-1;y>=0;y--){//look up 
    Color c=isBack ? Color.black : Color.white; 
    if(getChess(xIndex,y,c)!=null){ 
     continueCount++; 
    }else 
     break; 
   } 
-  for(int y=yIndex+1;y<=Config.ROWS;y++){//纵向向下寻找 
+  for(int y=yIndex+1;y<=Config.ROWS;y++){//look down 
    Color c=isBack ? Color.black : Color.white; 
    if(getChess(xIndex,y,c)!=null){ 
     continueCount++; 
    }else 
     break; 
   } 
-  if(continueCount>=5){//判断记录数大于等于五，即表示此方获胜 
+  if(continueCount>=6){
    return true; 
   }else 
    continueCount=1; 
   // 
-  for(int x=xIndex+1,y=yIndex-1;y>=0&&x<=Config.COLS;x++,y--){//右下寻找 
+  for(int x=xIndex+1,y=yIndex-1;y>=0&&x<=Config.COLS;x++,y--){
    Color c=isBack ? Color.black : Color.white; 
    if(getChess(x,y,c)!=null){ 
     continueCount++; 
    }else 
     break; 
   } 
-  for(int x=xIndex-1,y=yIndex+1;y<=Config.ROWS&&x>=0;x--,y++){//左上寻找 
+  for(int x=xIndex-1,y=yIndex+1;y<=Config.ROWS&&x>=0;x--,y++){ 
    Color c=isBack ? Color.black : Color.white; 
    if(getChess(x,y,c)!=null){ 
     continueCount++; 
    }else 
     break; 
   } 
-  if(continueCount>=5){//判断记录数大于等于五，即表示此方获胜 
+  if(continueCount>=6){ 
    return true; 
   }else 
    continueCount=1; 
   // 
-  for(int x=xIndex-1,y=yIndex-1;y>=0&&x>=0;x--,y--){//左下寻找 
+  for(int x=xIndex-1,y=yIndex-1;y>=0&&x>=0;x--,y--){
    Color c=isBack ? Color.black : Color.white; 
    if(getChess(x,y,c)!=null){ 
     continueCount++; 
    }else 
     break; 
   } 
-  for(int x=xIndex+1,y=yIndex+1;y<=Config.ROWS&&x<=Config.COLS;x++,y++){//右上寻找 
+  for(int x=xIndex+1,y=yIndex+1;y<=Config.ROWS&&x<=Config.COLS;x++,y++){ 
    Color c=isBack ? Color.black : Color.white; 
    if(getChess(x,y,c)!=null){ 
     continueCount++; 
    }else 
     break; 
   } 
-  if(continueCount>=5){//判断记录数大于等于五，即表示此方获胜 
+  if(continueCount>=6){
    return true; 
   }else 
    continueCount=1; 
@@ -199,13 +199,13 @@ public class ChessBoard extends JPanel implements MouseListener{
   } 
   return null; 
  } 
- public void restartGame(){//清除棋子 
+ public void restartGame(){//Clear chessman 
   for(int i=0;i<chessList.length;i++) 
    chessList[i]=null; 
-  /*恢复游戏相关的变量值*/
+  /*Restore game-related variables*/
   isBack=true; 
-  gameOver=false;//游戏是否结束 
-  chessCount=0;//当前棋盘的棋子个数 
+  gameOver=false;//Whether the game is over 
+  chessCount=0;//Number of chessmen on the current board
   repaint();  
  } 
  public void goback(){ 
@@ -220,7 +220,7 @@ public class ChessBoard extends JPanel implements MouseListener{
   isBack=!isBack; 
   repaint(); 
  } 
- //Dimension:矩形 
+ //Dimension:rectangle 
  public Dimension getPreferredSize(){ 
   return new Dimension((Config.MARGIN) *2+Config.GRID_SPAN*Config.COLS,Config.MARGIN*2+Config.GRID_SPAN*Config.ROWS); 
  } 
